d8a7b9c52b5eb4dc1ee4487b589325a72957858d
tejastej1997 <tejastej1997@gmail.com>
2023-03-14 17:03:01 +0530

    api modified

--- server.js
+++ server.js
@@ -31,12 +31,12 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
 
     try {
-        const { name, DOB, gender,age, contactnum, email, education, institute, standard, subject, totalfee, paidfee, duefee } = req.body;
+        const { name, DOB, gender, age, contactnum, email, education, institute, standard, subject, totalfee, paidfee, duefee } = req.body;
         const newStudent = new tutorialdetails({
             name: name,
             DOB: DOB,
             gender: gender,
-            age:age,
+            age: age,
             contactnum: contactnum,
             email: email,
             education: education,
@@ -56,13 +56,49 @@ app.post('/', async (req, res) => {
     }
 })
 
-app.delete('/:id',async(req,res)=>{
+app.get('/:id',async(req,res)=>{
+    return res.json(await tutorialdetails.find())
+})
+app.put('/update:id', async(req, res) => {
+    const { name, DOB, gender, age, contactnum, email, education, institute, standard, subject, totalfee, paidfee, duefee } = req.body;
+
     try{
+
+        const updatedStudent = new tutorialdetails({
+            _id:id,
+            name: name,
+            DOB: DOB,
+            gender: gender,
+            age: age,
+            contactnum: contactnum,
+            email: email,
+            education: education,
+            institute: institute,
+            standard: standard,
+            subject: subject,
+            totalfee: totalfee,
+            paidfee: paidfee,
+            duefee: duefee
+        })
+       await tutorialdetails.findByIdAndUpdate(id,updatedStudent)
+       return res.json( await tutorialdetails.find())
+    }
+    catch (err){
+        console.log(err);
+        
+    }
+
+
+})
+
+
+
+app.delete('/:id', async (req, res) => {
+    try {
         await tutorialdetails.findByIdAndDelete(req.params.id)
         return res.json(await tutorialdetails.find())
     }
-    catch(err)
-    {
+    catch (err) {
         console.log(err)
     }
 })
